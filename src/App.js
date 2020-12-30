2be0a370485962c170efdfa630e67aa4d03aa0b3
jyun1 <jyuny12345@gmail.com>
2020-12-30 12:00:01 +0800

    background set

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import styled from 'styled-components';
 import { color } from './style/color';
+import backgroundPattern from './assets/footer-background.svg'
 
 const Wrapper = styled.div`
+  height: 100%;
   display:flex;
+  flex-direction: column;
   justify-content: center;
-  h1{
-    color: ${color.main_background_color};
+  position: relative;
+
+  &::after{
+    content: '';
+    display:block;
+    width:100%;
+    height: 20%;
+    background-color: ${color.main_background_color};
+    background-image: url(${backgroundPattern});
+    background-position: 20px;
+    background-size: 4%;
+    position: absolute;
+    bottom:0;
+
   }
 `
 
 const App = () => {
   return (
     <Wrapper>
-      <h1>hello world</h1>
     </Wrapper>
   );
 }
